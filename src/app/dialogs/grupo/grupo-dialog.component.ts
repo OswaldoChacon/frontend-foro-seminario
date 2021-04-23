00d4fe40c4c3db146c60e10cba7fa41650cff240
Oswaldo de Jesus Chacon Perez <39079647+OswaldoChacon@users.noreply.github.com>
2021-04-23 00:35:39 -0500

    renombrar componente y arreglando input's del formulario

---
+++ src/app/dialogs/grupo/grupo-dialog.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Inject, Optional } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { FormBuilder,  Validators, FormControl } from "@angular/forms";
+import { Grupo } from 'src/app/modelos/grupo.model';
+import { GrupoService } from 'src/app/services/grupos/grupo.service';
+@Component({
+  selector: 'app-grupo',
+  templateUrl: './grupo-dialog.component.html',
+  styleUrls: ['./grupo-dialog.component.css']
+})
+export class GrupoDialogComponent implements OnInit {
+  formGrupo = this._formBuilder.group({
+    nombre: new FormControl("", [Validators.required]),
+    ponderacion: new FormControl("", [Validators.required]),
+    // plantilla_id: localStorage.plantilla_id
+  });
+  editar: boolean = false;
+
+  constructor(
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: Grupo,
+    private _formBuilder: FormBuilder,
+    private _GruposService: GrupoService,
+    private _dialogRef: MatDialogRef<GrupoDialogComponent>
+  ) { _dialogRef.disableClose = true; }
+
+  ngOnInit(): void {
+    if (this.data != null) {
+      this.editar = true;
+      this.cargarDatosFormulario();
+    }
+  }
+
+  cargarDatosFormulario(){
+    this.formGrupo.setValue({
+      nombre: this.data.nombre,
+      ponderacion: this.data.ponderacion,      
+    });
+  }
+
+  registrarGrupo() {
+    this._GruposService.guardarGrupos(this.formGrupo, localStorage.plantilla_id).subscribe(() => this._dialogRef.close({ opcion: 'refresh' }));
+ }
+
+ actualizarGrupo() {
+  this._GruposService.actualizarGrupo(this.data.id, localStorage.plantilla_id, this.formGrupo).subscribe(() => this._dialogRef.close({ opcion: 'refresh' }));
+}
+
+}
