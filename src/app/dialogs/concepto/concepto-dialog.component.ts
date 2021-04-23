f05289cdda935408e944ca20e7702e813baa82e0
Oswaldo de Jesus Chacon Perez <39079647+OswaldoChacon@users.noreply.github.com>
2021-04-23 00:34:17 -0500

    renombrando componente y arreglando detalles en el formulario

--- src/app/dialogs/concepto/concepto-dialog.component.ts
+++ src/app/dialogs/concepto/concepto-dialog.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
-import { FormBuilder,  Validators, FormControl } from "@angular/forms";
+import { FormBuilder, Validators, FormControl } from "@angular/forms";
 import { Concepto } from 'src/app/modelos/concepto.model';
-import { ConceptoService } from 'src/app/services/conceptos/conceptos.service';
+import { ConceptoService } from 'src/app/services/conceptos/concepto.service';
 import { Inject } from '@angular/core';
 import { Optional } from '@angular/core';
 @Component({
   selector: 'app-concepto',
-  templateUrl: './concepto.component.html',
+  templateUrl: './concepto-dialog.component.html',
 })
 export class ConceptoDialogComponent implements OnInit {
 
- 
+
   formConcepto = this._formBuilder.group({
-    conceptos: new FormControl("", [Validators.required]),
+    nombre: new FormControl("", [Validators.required]),
     ponderacion: new FormControl("", [Validators.required]),
     seminario: 0,
-    grupo_id: localStorage.grupo_id,
-  });  
-  editar: boolean = false; 
+  });
+  // grupo_id: localStorage.grupo_id,
+  editar: boolean = false;
 
   constructor(
     @Optional() @Inject(MAT_DIALOG_DATA) public data: Concepto,
@@ -28,29 +28,29 @@ export class ConceptoDialogComponent implements OnInit {
   ) { _dialogRef.disableClose = true; }
 
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
     if (this.data != null) {
       this.editar = true;
       this.cargarDatosFormulario();
     }
-  }  
-  
-  guardarConcepto() {    
-     this._ConceptoService.guardarConcepto(this.formConcepto).subscribe(() => this._dialogRef.close({ opcion: 'refresh' }));
   }
 
-  actualizarConcepto() {    
-     this._ConceptoService.actualizarConcepto(this.data.id, this.formConcepto).subscribe(() => this._dialogRef.close({ opcion: 'refresh' }));
+  guardarConcepto() {
+    this._ConceptoService.guardarConcepto(this.formConcepto, localStorage.grupo_id).subscribe(() => this._dialogRef.close({ opcion: 'refresh' }));
   }
 
-  cargarDatosFormulario(){
+  actualizarConcepto() {
+    this._ConceptoService.actualizarConcepto(this.data.id, this.formConcepto, localStorage.grupo_id).subscribe(() => this._dialogRef.close({ opcion: 'refresh' }));
+  }
+
+  cargarDatosFormulario() {
     this.formConcepto.setValue({
-      conceptos: this.data.conceptos,
+      nombre: this.data.nombre,
       ponderacion: this.data.ponderacion,
       seminario: 0,
       grupo_id: localStorage.grupo_id,
 
-    });    
+    });
   }
 
 
